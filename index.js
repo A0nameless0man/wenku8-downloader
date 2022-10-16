bcdb873f91a83cdcb7e1383dd56d3acff309eeac
huguang@WSL.HG-Desktop <1395943920@qq.com>
2022-10-17 03:38:28 +0800

    new algo

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const Novel = require("./lib/downloader.js");
 
 const { program } = require("commander");
+const log4js = require("log4js");
+
+const logger = log4js.getLogger();
+logger.level = "debug";
 
 function commaSeparatedList(value) {
     return value.split(',');
@@ -13,14 +17,21 @@ program
     .option("-u, --url <type>", "website url of the novel you want to download")
     .option("-i, --id <int>", "website id of the novel you want to download", commaSeparatedList);
 program.parse(process.argv);
-
+logger.warn("开始下载")
 if (program.url) {
     Novel.download(program.url);
 } else if (program.id) {
     let s = new Set(program.id)
-    s.forEach(element => {
-        Novel.download(`https://www.wenku8.net/book/${element}.htm`)
-    });
+    const p = Promise.all(Array.from(s.keys()).map(
+        element => {
+            const url = `https://www.wenku8.net/book/${element}.htm`
+            return Novel.download(url)
+        }
+    ))
+    p.then(() => {
+        logger.info("下载完毕")
+        Novel.clean()
+    })
 } else {
-    console.log("请使用-u标志输入URL");
+    logger.warn("请使用-u标志输入URL");
 }
